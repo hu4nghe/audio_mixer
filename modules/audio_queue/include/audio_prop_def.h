4f55b79752cb0b09ce496c46e8e39c2de0bcfeb0
HUANG He <he.hu4ng@outlook.com>
2025-10-04 13:03:39 +0200

    feat(audio_prop_def): Operation check
    add function to check if input audio needs resample or channel mapping

--- modules/audio_queue/include/audio_prop_def.h
+++ modules/audio_queue/include/audio_prop_def.h
@@ -1,24 +1,53 @@
-#include <cstdint>
+/**
+ * @file audio_prop_def.h
+ * @author  ()
+ * @brief 
+ * @version 0.1
+ * @date 2025-10-03
+ * 
+ * @copyright Copyright (c) 2025
+ * 
+ */
+#pragma once
 
-enum class channel_layout : std::uint8_t 
-{
-    mono            = 1,   // 1.0 
-    stereo          = 2,   // 2.0 
-    five_point_one  = 6,   // 5.1 
-    seven_point_one = 8, // 7.1
-};
-
 #include <cstdint>
+#include <optional>
+#include <utility>
+
+#include "channel.h"
+
 enum class sample_rate : std::uint32_t 
 {
-    SR8000   = 8000,
-    SR16000  = 16000,
-    SR22050  = 22050,
-    SR32000  = 32000,
     SR44100  = 44100,
     SR48000  = 48000,
     SR88200  = 88200,
     SR96000  = 96000,
     SR176400 = 176400,
     SR192000 = 192000
+};
+
+struct audio_ctx
+{
+    sample_rate     _sample_rate = sample_rate::SR44100;
+    channel_layout  _channel_num = channel_layout::stereo;
+    
+    auto need_conversion(const audio_ctx& other) const
+    -> std::optional<channel_layout::matrix_type> 
+    {
+        if(_channel_num != other._channel_num)
+            // Convert from other(usually input) to me(usually expected)
+            return other._channel_num.matrix_to(_channel_num);
+        else 
+            return std::nullopt;
+    }
+
+    auto need_resample(const audio_ctx& other) const
+    -> std::optional<double>
+    {
+        if(_sample_rate != other._sample_rate)
+            // Convert from other(usually input) to me(usually expected)
+            return std::to_underlying(_sample_rate) / std::to_underlying(other._sample_rate);
+        else
+            return std::nullopt;
+    }
 };
