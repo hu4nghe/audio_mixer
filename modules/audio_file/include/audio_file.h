17b031f0bbeab6e992e64ce3b5cd195f03d8ae24
HUANG He <he.hu4ng@outlook.com>
2025-10-12 13:09:32 +0200

    init(audio_file): audio_file module init

---
+++ modules/audio_file/include/audio_file.h
@@ -0,0 +1,13 @@
+/**
+ * @file audio_file.h
+ * @author HUANG He (he.hu4ng@outlook.com)
+ * @brief audio mixer module to read from an audio file.
+ * @version 0.1
+ * @date 2025-10-12
+ * 
+ * @copyright Copyright (c) 2025
+ * 
+ */
+#pragma once
+
+#include "sndfile.h"
