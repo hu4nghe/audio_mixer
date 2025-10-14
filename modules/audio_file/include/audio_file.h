251f0b1ebfbb194d2c6d8dff7c5a597ca9f5ac4e
HUANG He <he.hu4ng@outlook.com>
2025-10-14 18:47:02 +0200

    feat(audio_file): Add read_file fucntion
    Add read_file function for audio_file module for read audio/video file.

--- modules/audio_file/include/audio_file.h
+++ modules/audio_file/include/audio_file.h
@@ -10,4 +10,64 @@
  */
 #pragma once
 
+#include "audio_prop_def.h"
 #include "sndfile.h"
+#include "input_module_base.h"
+#include <iostream>
+#include <memory>
+#include <string>
+#include <filesystem>
+
+namespace fs = std::filesystem;
+
+template <AudioSampleType AudioType>
+class audio_file : public input_module_base<AudioType>
+{
+    std::vector<fs::path> m_audios;
+	std::vector<fs::path> m_videos;
+
+public : 
+
+    audio_file(std::shared_ptr<audio_queue<AudioType>> target_queue)
+        : input_module_base<AudioType>(target_queue,false){}
+    
+
+    void select_file()
+    {
+        std::println("Please enter the path of the sound file.\nE(nd)");
+
+        std::string file_path_str;
+        while (true)
+        {
+            std::getline(std::cin >> std::ws, file_path_str);
+            
+            if (file_path_str == "E" || file_path_str == "e" )
+            {
+                std::println("File lists confirmed.");
+                break;
+            }
+
+            fs::path file_path(file_path_str);
+            if (!fs::exists(file_path))
+            {
+                std::println("No such file or directory.");
+                continue;
+            }
+
+            const auto extension = file_path.extension();
+            
+            if (extension == ".wav")
+                m_audios.push_back(file_path);
+            else if (extension == ".mov" || extension == ".mp4")
+                m_videos.push_back(file_path);
+            
+            else
+            {
+                std::println("Format not supported.");
+                continue;
+            }
+
+            std::println("File selected: {}.", file_path.filename().string());
+        }
+    }
+};
